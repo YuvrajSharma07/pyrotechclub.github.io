daeabb29feee2954e4408181edd0528784a8063d
Yuv <billiondollarone07@gmail.com>
2017-05-12 19:14:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var undl = document.getElementById('undl');
-setTimeout(function () { "use strict"; undl.className = "visible"; }, 3000);
+setTimeout(function () {
+    $("div#animate").removeClass("hide").addClass("show");
+}, 1000);
+
+document.getElementById('kmore').onclick = function () {
+	$("div#intro").addClass("fadeaway");
+	$("body").removeClass("bodybg");
+	setTimeout(function () {document.getElementById('intro').style.display = "none"; }, 1800);
+	setTimeout(function () {document.getElementById('mainbod').style.display = "block"; }, 3000);
+};
