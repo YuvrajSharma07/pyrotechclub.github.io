32ad34ff89d1e42328092ba7c43daca5811d5167
Yuv <billiondollarone07@gmail.com>
2017-05-13 18:36:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ document.getElementById('kmore').onclick = function () {
 	setTimeout(function () {document.getElementById('intro').style.display = "none"; }, 1800);
 	setTimeout(function () {document.getElementById('mainbod').style.display = "block"; }, 3000);
 };
+$(window).scroll(function() {
+if ($(this).scrollTop() > 100){  
+    $('#mainheader').addClass("scrhead");
+  }
+  else{
+    $('#mainheader').removeClass("scrhead");
+  }
+});
